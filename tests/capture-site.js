3a6f4e5bd75eac0cea12af1bc37934472410ff8b
Erik Stockmeier <erik.d.stock@gmail.com>
2017-02-09 17:16:23 -0500

    add pages

--- tests/capture-site.js
+++ tests/capture-site.js
@@ -4,10 +4,23 @@ module.exports = {
     var mergedUrl = 'https://merged.artsy.net'
     var microgravityUrl = 'https://m.artsy.net'
     var screenshotPath = './screenshots/'
+
     var pages = {
       home: '/',
-      auctions: '/auctions'
+      auctions: '/auctions',
+      artists: '/artists',
+      collect: '/collect',
+      shows: '/shows',
+      galleries: '/galleries',
+      institutions: '/institutions',
+      // admin: '/admin',
+      // search: '/admin',
+      auctionItem: '/artwork/yaacov-agam-untitled-iv',
+      auctionPage: '/auction/heritage-auctions-valentines-day-prints-and-multiples',
+      bid: '/auction/heritage-auctions-valentines-day-prints-and-multiples/bid/yaacov-agam-untitled-iv',
+
     }
+
     _.each(pages, function (location, pageName) {
       browser
         .url(mergedUrl + location)
