db0cb97d1fa077467289ef04740916bf09660912
Erik Stockmeier <erik.d.stock@gmail.com>
2017-02-09 13:33:44 -0500

    basic setup

---
+++ nightwatch.conf.js
@@ -0,0 +1,37 @@
+module.exports = {
+  "src_folders" : ["tests"],
+  "output_folder" : "reports",
+  "custom_commands_path" : "",
+  "custom_assertions_path" : "",
+  "page_objects_path" : "",
+  "globals_path" : "./setup/globals.js",
+
+  "selenium" : {
+    "start_process" : false,
+  },
+
+  "test_settings" : {
+    "default" : {
+      "selenium_port"  : 9515,
+      "selenium_host"  : "localhost",
+      "default_path_prefix" : "",
+      "screenshots" : {
+        "enabled" : true,
+        "path" : "./screenshots"
+      },
+
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "chromeOptions" : {
+          "args" : ["--no-sandbox"]
+        },
+        "acceptSslCerts": true
+      }
+    },
+    "chromeMobile" : { // FIXME: make actually mobile
+      "desiredCapabilities": {
+        "browserName": "chrome"
+      }
+    }
+  },
+}
